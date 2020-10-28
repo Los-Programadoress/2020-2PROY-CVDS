e67146df6a71115d4563014d993bd4e38ebcab7f
Laura Alejandra Izquierdo Castro <alizeci17@gmail.com>
2020-10-27 23:08:57 -0500

    Configuración base de datos

--- src/main/java/edu/eci/cvds/persistence/UsuarioDAO.java
+++
@@ -1,8 +0,0 @@
-package edu.eci.cvds.persistence;
-
-import edu.eci.cvds.entities.Usuario;
-
-public interface UsuarioDAO {
-	
-	public Usuario consultarUsuario(String idCorreo, String contrasena) throws PersistenceException;
-}
