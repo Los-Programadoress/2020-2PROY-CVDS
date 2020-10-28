430d3d0ab2e087c9924f6e773c26298502df0895
Angelica Alfaro <maria.alfaro@mail.escuelaing.edu.co>
2020-10-27 22:38:08 -0500

    Ajustando gitignore

--- historialdeequipos/src/main/java/edu/eci/cvds/services/EquiposServices.java
+++
@@ -1,8 +0,0 @@
-package edu.eci.cvds.services;
-
-import edu.eci.cvds.entities.Usuario;
-
-public interface EquiposServices {
-	
-	public Usuario consultarUsuario(String idCorreo, String contrasena) throws EquiposException;
-}
