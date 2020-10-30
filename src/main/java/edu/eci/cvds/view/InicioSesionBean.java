9053a9eb7ed4b70774cdcc418daf7031e8752052
Laura Alejandra Izquierdo Castro <alizeci17@gmail.com>
2020-10-29 23:28:35 -0500

    Apache Shiro Implementado

--- src/main/java/edu/eci/cvds/view/InicioSesionBean.java
+++ src/main/java/edu/eci/cvds/view/InicioSesionBean.java
@@ -1,111 +1,113 @@
 package edu.eci.cvds.view;
 
-import java.io.IOException;
-import java.io.Serializable;
-
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import javax.ejb.Stateless;
 import javax.faces.application.FacesMessage;
-import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
-
-import org.apache.shiro.SecurityUtils;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.logging.Level;
+import javax.faces.bean.ManagedBean;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.LockedAccountException;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.crypto.hash.Sha256Hash;
-import org.apache.shiro.subject.Subject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Named
 @Stateless
 @SessionScoped
-@ManagedBean(name = "inicioSesionBean", eager = true)
-public class InicioSesionBean{
+@ManagedBean(name = "shiroBean", eager = true)
+public class InicioSesionBean implements Serializable{
 
-	private static final Logger log = LoggerFactory.getLogger(InicioSesionBean.class);
-	private String urlLogin = "/faces/index.xhtml";
-	private Boolean rememberMe = false;
-	private String idCorreo;
+    private static final Logger log = LoggerFactory.getLogger(InicioSesionBean.class);
+
+    private String idCorreo;
     private String password;
-	private String message;
-	Subject subject;
-	
-	/**
+    private Boolean rememberMe = false;
+    private String redirectUrl = "/faces/index.xhtml";
+    Subject subject;
+
+    /**
      * Try and authenticate the user
      */
     public void doLogin() {
-    	subject = SecurityUtils.getSubject();
-        UsernamePasswordToken token = new UsernamePasswordToken(getidCorreo(), new Sha256Hash(getPassword()).toHex());
-		
+    	//System.out.println(getidCorreo());
+    	//System.out.println(getPassword());
+        subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken(getidCorreo(), getPassword());
         try {
-        	subject.login(token);
-			if (subject.hasRole("Estudiante")) {
-				System.out.println("entra");
-				FacesContext.getCurrentInstance().getExternalContext().redirect("/faces/sesion.xhtml");
-			}
-        } catch (UnknownAccountException e1) {
+            subject.login(token);
+            if (subject.hasRole("Estudiante")) {
+                FacesContext.getCurrentInstance().getExternalContext().redirect("/sesion.xhtml");
+            } else if (subject.hasRole("Profesor")) {
+                FacesContext.getCurrentInstance().getExternalContext().redirect("/sesion.xhtml");
+            }
+        } catch (UnknownAccountException ex) {
+        	System.err.println("Cuenta Desconocida");
             facesError("Cuenta Desconocida");
-            log.error(e1.getMessage(), e1);
-        } catch (IncorrectCredentialsException e2) {
-        	System.err.println("Incorrecto");
-            facesError("Contraseña incorrecta");
-            log.error(e2.getMessage(), e2);
-        } catch (LockedAccountException e3) {
-            facesError("Cuenta bloqueada");
-            log.error(e3.getMessage(), e3);
-        } catch (AuthenticationException | IOException e4) {
-            facesError("Error Desconocido: " + e4.getMessage());
-            log.error(e4.getMessage(), e4);
-        } catch (NullPointerException e5) {
+            log.error(ex.getMessage(), ex);
+        } catch (IncorrectCredentialsException ex) {
+            System.err.println("Contraseña Incorrecta");
+            facesError("Contraseña Incorrecta");
+            log.error(ex.getMessage(), ex);
+        } catch (LockedAccountException ex) {
+        	System.err.println("Cuenta Inactiva");
+            facesError("Cuenta Inactiva");
+            log.error(ex.getMessage(), ex);
+        } catch (AuthenticationException | IOException ex) {
+        	System.err.println("Error Desconocido:");
+            facesError("Error Desconocido: " + ex.getMessage());
+            log.error(ex.getMessage(), ex);
+        } catch (NullPointerException e) {
             System.err.println("Nulo");
         } finally {
             token.clear();
         }
     }
-	
-	public void doLogOut() throws  IOException{
+
+    public void doLogOut() {
+
         SecurityUtils.getSubject().logout();
         try {
-            FacesContext.getCurrentInstance().getExternalContext().redirect(urlLogin);
+            FacesContext.getCurrentInstance().getExternalContext().redirect(redirectUrl);
         } catch (IOException ex) {
-            java.util.logging.Logger.getLogger(InicioSesionBean.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(InicioSesionBean.class.getName()).log(Level.SEVERE, null, ex);
         }
-	}
-	
-	/**
+    }
+
+    /**
      * Adds a new SEVERITY_ERROR FacesMessage
+     *
      * @param message Error Message
      */
     private void facesError(String message) {
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, message, null));
     }
 
-	public String getidCorreo() {
-		return idCorreo;
-	}
+    public String getidCorreo() {
+        return idCorreo;
+    }
 
-	public void setidCorreo(String idCorreo) {
-		this.idCorreo = idCorreo;
-	}
+    public void setidCorreo(String login) {
+        this.idCorreo = login;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String senha) {
+        this.password = senha;
+    }
 
-	public String getMessage() {
-		return message;
-	}
-	
-	public Boolean getRememberMe() {
+    public Boolean getRememberMe() {
         return rememberMe;
     }
 
@@ -113,16 +115,11 @@ public class InicioSesionBean{
         this.rememberMe = lembrar;
     }
 
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	
-	public String getUrl() {
-        return urlLogin;
+    public String getRedirectUrl() {
+        return redirectUrl;
     }
 
-    public void setUrl(String urlLogin) {
-        this.urlLogin = urlLogin;
+    public void setRedirectUrl(String redirectUrl) {
+        this.redirectUrl = redirectUrl;
     }
-	
 }
