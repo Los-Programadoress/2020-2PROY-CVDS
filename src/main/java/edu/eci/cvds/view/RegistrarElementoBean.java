3ebc1b70ab29a3505f97e71a7fe5928311f3b600
Laura Alejandra Izquierdo Castro <alizeci17@gmail.com>
2020-11-04 10:12:26 -0500

    Bug de Consultar Elemento solucionado

--- src/main/java/edu/eci/cvds/view/RegistrarElementoBean.java
+++ src/main/java/edu/eci/cvds/view/RegistrarElementoBean.java
@@ -8,45 +8,42 @@ import javax.faces.bean.SessionScoped;
 import com.google.inject.Inject;
 
 import edu.eci.cvds.entities.Elemento;
-import edu.eci.cvds.persistence.PersistenceException;
 import edu.eci.cvds.services.EquiposException;
 import edu.eci.cvds.services.EquiposServices;
 
-@ManagedBean(name="RegistrarElementoBean")
+@ManagedBean(name="registrarElementosBean")
 @SessionScoped
-public class RegistrarElementoBean {
-	 private List<Elemento> elementos;
+public class RegistrarElementoBean extends BasePageBean{
+	 
+	private static final long serialVersionUID = 328623664005639438L;
+
+	private List<Elemento> elementos = null;
 	 
 	 @Inject
 	 private EquiposServices equipoS;
 
 	public List<Elemento> consultarElementos(){
-		System.out.println("entro al bean");
 		try{
 			elementos = equipoS.consultarElementos();
-			System.out.println(elementos.size());
-		}
-		catch(EquiposException e){   
-			System.out.println(elementos.size());
+		}catch(EquiposException e){   
+			e.printStackTrace();
 		}
 		
 		return elementos; 
 	 }  
+	
 	 
 	 public void registrarElementoEquipo(String tipo, String nombre, int nequipo) throws EquiposException{
     	try {
    		 equipoS.registrarElementoEquipo(tipo,nombre,nequipo);
- 		}
- 		catch (EquiposException e) {
+ 		}catch (EquiposException e) {
+ 			e.printStackTrace();
    	 	}
 	 }
 	 
-	 public void registrarElemento(String tipo, String nombre) throws EquiposException{
-		try{
-			equipoS.registrarElemento(tipo, nombre);
-		}
-		catch(EquiposException e){           
-		}
+	 public void registrarElemento( String nombre) throws EquiposException{
+		 
+		 System.out.println("entro a elemento resgister");
 	 }
 	 
 	 public List<Elemento> getElementos() {
