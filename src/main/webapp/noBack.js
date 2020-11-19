02d8f0bf50bdeab615b392deaaa514d8219e32b1
Laura Alejandra Izquierdo Castro <alizeci17@gmail.com>
2020-11-18 21:01:07 -0500

    Commit Ajuste

--- src/main/webapp/noBack.js
+++
@@ -1,8 +0,0 @@
-window.onload = function(){
-	window.location.hash = "no-back-button";
-	window.location.hash = "Again-No-back-button";
-	
-	window.onhashchange= function(){
-		window.location.hash = "no-back-button";
-	}
-}
